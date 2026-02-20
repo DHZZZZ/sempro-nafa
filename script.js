4f2d93c85b1978979586c5709fec4593190b7f6c
DHZZZZ <96244328+DHZZZZ@users.noreply.github.com>
2026-02-21 04:43:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const photos = document.querySelectorAll('.photo');
+let index = 0;
+
+setInterval(() => {
+    photos.forEach(photo => {
+        photo.classList.remove('active', 'fall');
+    });
+
+    if (index > 0) {
+        photos[index - 1].classList.add('fall');
+    } else {
+        photos[photos.length - 1].classList.add('fall');
+    }
+
+    photos[index].classList.add('active');
+
+    index++;
+    if (index >= photos.length) {
+        index = 0;
+    }
+}, 3000);
