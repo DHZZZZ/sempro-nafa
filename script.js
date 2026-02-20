7b50fdfebe065a3360b9c9837bc57071f7378b33
DHZZZZ <96244328+DHZZZZ@users.noreply.github.com>
2026-02-21 05:41:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ const photos = document.querySelectorAll('.photo');
 let index = 0;
 
 setInterval(() => {
-    photos.forEach(photo => {
-        photo.classList.remove('active', 'fall');
-    });
-
-    const prevIndex = index === 0 ? photos.length - 1 : index - 1;
-    photos[prevIndex].classList.add('fall');
+    photos.forEach(photo => photo.classList.remove('active'));
     photos[index].classList.add('active');
-
     index = (index + 1) % photos.length;
-}, 3200);
+}, 3500);
